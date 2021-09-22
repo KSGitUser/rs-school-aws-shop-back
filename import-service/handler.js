867f52112863484002318e087b6c8683a2c8bf91
Sergey Krikun <sergey.a.krikun@gmail.com>
2021-09-23 00:54:02 +0300

    feat: add importProductsFile:

--- import-service/handler.js
+++ import-service/handler.js
@@ -2,22 +2,34 @@ const AWS = require('aws-sdk');
 const BUCKET = 'rsschool-s3-service';
 
 module.exports = {
-  getFiles: async function () {
+  importProductsFile: async function (event) {
+
     const s3 = new AWS.S3({ region: 'eu-west-1' });
     let status = 200;
-    let products = [];
-    const params = {
-      Bucket: BUCKET,
-      Prefix: 'files/'
-    };
 
-    try {
-      const s3Response = await s3.listObjectsV2(params).promise();
-      products = s3Response.Contents;
-    } catch (error) {
-      console.error(error);
+    const queryName = event.queryStringParameters.name;
+
+    if (!queryName) {
       status = 500;
     }
+
+    const catalogPath = `uploaded/${queryName}`;
+    const params = {
+      Bucket: BUCKET,
+      Key: catalogPath,
+      Expires: 120,
+      ContentType: 'text/csv'
+    }
+
+    const getResponse = async (error, url, resolve) => {
+      if (error) {
+        status = 500;
+        return error;
+      }
+      return url;
+    }
+
+    const responseBody = await new Promise(resolve => s3.getSignedUrl('putObject', params, (error, url) => resolve(getResponse(error, url))));
 
     const response = {
       statusCode: status,
@@ -25,17 +37,31 @@ module.exports = {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': true,
       },
-      body: JSON.stringify(
-        products
-          .filter(product => product.Size)
-          .map(product => {
-            return `https://${BUCKET}.s3.amazonaws.com/${product.Key}`
-
-          })
-      )
+      body: responseBody
     }
 
     return response;
+  },
+  importFileParser: async function (event) {
+    console.log('event =>', event);
+    const s3 = new AWS.S3({ region: 'eu-west-1' });
+
+    for (const record of event.Records) {
+      await s3.copyObject({
+        Bucket: BUCKET,
+        CopySource: BUCKET + '/' + record.s3.object.key,
+        Key: record.s3.object.key.replace('import', 'uploaded')
+      }).promise();
+
+
+      await s3.deleteObject({
+        Bucket: BUCKET,
+        Key: record.s3.object.key
+      }).promise();
+
+      console.log(`Uploaded file ${record.s3.object.key} is created`);
+
+    }
   }
 
 }
